f7a2e9c8ee9a0ef88d76951bfdd5ec5984dc5895
SoufianeAbk <229384282+sso-vbk@users.noreply.github.com>
2025-09-30 00:14:10 +0200

    Nieuwe klasse BestellingEventArgs met Id, naam en prijs

--- BoekWinkelBestellingSysteem/Orders/BestelllingEventsArgs.cs
+++ BoekWinkelBestellingSysteem/Orders/BestelllingEventsArgs.cs
@@ -4,9 +4,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BoekWinkelBestellingSysteem.Orders
+using System;
+
+namespace BoekwinkelBestellingssysteem.Orders
 {
-    internal class BestelllingEventsArgs
+    public class BestellingEventArgs : EventArgs
     {
+        public int BestellingId { get; set; }
+        public string ItemNaam { get; set; }
+        public decimal TotaalPrijs { get; set; }
+
+        public BestellingEventArgs(int id, string naam, decimal prijs)
+        {
+            BestellingId = id;
+            ItemNaam = naam;
+            TotaalPrijs = prijs;
+        }
     }
 }
